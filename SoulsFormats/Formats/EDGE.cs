ab62cc6631ed9415f7dd9b40c973854840c455ea
Joseph Anderson <josepha93@gmail.com>
2019-06-12 22:19:53 -0400

    EDGE: figured out some things

--- SoulsFormats/Formats/EDGE.cs
+++ SoulsFormats/Formats/EDGE.cs
@@ -17,7 +17,12 @@ namespace SoulsFormats
         /// <summary>
         /// Edges defined in this file.
         /// </summary>
-        public List<Edge> Edges;
+        public List<Edge> Edges { get; set; }
+
+        public EDGE()
+        {
+            Edges = new List<Edge>();
+        }
 
         internal override bool Is(BinaryReaderEx br)
         {
@@ -50,7 +55,28 @@ namespace SoulsFormats
         }
 
         /// <summary>
-        /// A grapple point or hangable edge.
+        /// Which type of edge an edge is.
+        /// </summary>
+        public enum EdgeType : byte
+        {
+            /// <summary>
+            /// A grapplable point.
+            /// </summary>
+            Grapple = 1,
+
+            /// <summary>
+            /// A hangable ledge.
+            /// </summary>
+            Hang = 2,
+
+            /// <summary>
+            /// A huggable wall.
+            /// </summary>
+            Hug = 3,
+        }
+
+        /// <summary>
+        /// A grapple point, hangable ledge, or huggable wall.
         /// </summary>
         public class Edge
         {
@@ -65,24 +91,50 @@ namespace SoulsFormats
             public Vector3 V2 { get; set; }
 
             /// <summary>
+            /// Only for wires, the point you're actually pulled towards.
+            /// </summary>
+            public Vector3 V3 { get; set; }
+
+            /// <summary>
+            /// Only for wires, unknown, always 1.
+            /// </summary>
+            public float Unk2C { get; set; }
+
+            /// <summary>
             /// Unknown.
             /// </summary>
             public int Unk30 { get; set; }
 
             /// <summary>
-            /// Unknown.
+            /// What type of edge this is.
             /// </summary>
-            public byte Unk34 { get; set; }
+            public EdgeType Type { get; set; }
 
             /// <summary>
-            /// Unknown.
+            /// For wires, a relative ID in WireVariationParam; for walls, unknown.
             /// </summary>
-            public byte Unk35 { get; set; }
+            public byte VariationID { get; set; }
 
             /// <summary>
             /// Unknown.
             /// </summary>
             public byte Unk36 { get; set; }
+
+            /// <summary>
+            /// Creates an Edge with default values.
+            /// </summary>
+            public Edge()
+            {
+                Type = EdgeType.Grapple;
+            }
+
+            /// <summary>
+            /// Clones an existing Edge.
+            /// </summary>
+            public Edge Clone()
+            {
+                return (Edge)MemberwiseClone();
+            }
 
             internal Edge(BinaryReaderEx br)
             {
@@ -90,13 +142,15 @@ namespace SoulsFormats
                 br.AssertSingle(1);
                 V2 = br.ReadVector3();
                 br.AssertSingle(1);
-                br.AssertPattern(0x10, 0x00);
+                V3 = br.ReadVector3();
+                Unk2C = br.ReadSingle();
                 Unk30 = br.ReadInt32();
-                Unk34 = br.ReadByte();
-                Unk35 = br.ReadByte();
+                Type = br.ReadEnum8<EdgeType>();
+                VariationID = br.ReadByte();
                 Unk36 = br.ReadByte();
                 br.AssertByte(0);
-                br.AssertPattern(0x8, 0x00);
+                br.AssertInt32(0);
+                br.AssertInt32(0);
             }
 
             internal void Write(BinaryWriterEx bw)
@@ -105,13 +159,26 @@ namespace SoulsFormats
                 bw.WriteSingle(1);
                 bw.WriteVector3(V2);
                 bw.WriteSingle(1);
-                bw.WritePattern(0x10, 0x00);
+                bw.WriteVector3(V3);
+                bw.WriteSingle(Unk2C);
                 bw.WriteInt32(Unk30);
-                bw.WriteByte(Unk34);
-                bw.WriteByte(Unk35);
+                bw.WriteByte((byte)Type);
+                bw.WriteByte(VariationID);
                 bw.WriteByte(Unk36);
                 bw.WriteByte(0);
-                bw.WritePattern(0x8, 0x00);
+                bw.WriteInt32(0);
+                bw.WriteInt32(0);
+            }
+
+            /// <summary>
+            /// Returns relevant information about the edge as a string.
+            /// </summary>
+            public override string ToString()
+            {
+                if (Type == EdgeType.Grapple)
+                    return $"{Type} Var:{VariationID} {Unk30} {Unk36} {V1} {V2} {V3}";
+                else
+                    return $"{Type} Var:{VariationID} {Unk30} {Unk36} {V1} {V2}";
             }
         }
     }
