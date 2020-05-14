560466d8adccf27e3f74abea90ff6e9aa753f921
JKAnderson <josepha93@gmail.com>
2020-05-14 00:17:28 -0400

    IMsb: summaries, finally

--- SoulsFormats/Formats/MSB/IMsb.cs
+++ SoulsFormats/Formats/MSB/IMsb.cs
@@ -3,50 +3,108 @@ using System.Numerics;
 
 namespace SoulsFormats
 {
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+    /// <summary>
+    /// A generic map layout file.
+    /// </summary>
     public interface IMsb
     {
+        /// <summary>
+        /// Models available for use in the map.
+        /// </summary>
         IMsbParam<IMsbModel> Models { get; }
 
+        /// <summary>
+        /// Events that control miscellaneous behaviors of the map.
+        /// </summary>
         IMsbParam<IMsbEvent> Events { get; }
 
+        /// <summary>
+        /// Points and trigger volumes in the map.
+        /// </summary>
         IMsbParam<IMsbRegion> Regions { get; }
 
+        /// <summary>
+        /// Concrete entities in the map.
+        /// </summary>
         IMsbParam<IMsbPart> Parts { get; }
     }
 
+    /// <summary>
+    /// A generic container of MSB items.
+    /// </summary>
     public interface IMsbParam<T> where T : IMsbEntry
     {
+        /// <summary>
+        /// Adds the given item to the appropriate part of the param.
+        /// </summary>
         T Add(T item);
 
+        /// <summary>
+        /// Returns all of the items in the param.
+        /// </summary>
         IReadOnlyList<T> GetEntries();
     }
 
+    /// <summary>
+    /// A generic item in an MSB param.
+    /// </summary>
     public interface IMsbEntry
     {
+        /// <summary>
+        /// The name of this entry.
+        /// </summary>
         string Name { get; set; }
     }
 
+    /// <summary>
+    /// A model available to be used by parts.
+    /// </summary>
     public interface IMsbModel : IMsbEntry { }
 
+    /// <summary>
+    /// Controls miscellaneous behaviors.
+    /// </summary>
     public interface IMsbEvent : IMsbEntry { }
 
+    /// <summary>
+    /// A point or trigger volume.
+    /// </summary>
     public interface IMsbRegion : IMsbEntry
     {
+        /// <summary>
+        /// The center or bottom center of the region, depending on its shape.
+        /// </summary>
         Vector3 Position { get; set; }
 
+        /// <summary>
+        /// The rotation of the region, in degrees.
+        /// </summary>
         Vector3 Rotation { get; set; }
     }
 
+    /// <summary>
+    /// A visible and/or physical entity.
+    /// </summary>
     public interface IMsbPart : IMsbEntry
     {
+        /// <summary>
+        /// The model used by this part.
+        /// </summary>
         string ModelName { get; set; }
 
+        /// <summary>
+        /// The position of the part.
+        /// </summary>
         Vector3 Position { get; set; }
 
+        /// <summary>
+        /// The rotation of the part, in degrees.
+        /// </summary>
         Vector3 Rotation { get; set; }
 
+        /// <summary>
+        /// The scale of the part.
+        /// </summary>
         Vector3 Scale { get; set; }
     }
-#pragma warning restore CS1591 // Missing XML comment for publicly visible type or member
 }
