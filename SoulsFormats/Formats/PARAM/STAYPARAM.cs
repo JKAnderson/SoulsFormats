6c122864a427205c9c48956511623430b97c66cb
Gary Tierney <gary.tierney@fastmail.com>
2021-04-03 20:15:19 +0100

    STAYPARAMS: Support reading STAYPARAM(DEF)s

---
+++ SoulsFormats/Formats/PARAM/STAYPARAM.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoulsFormats
+{
+    /// <summary>
+    /// A system configuration file to set the properties for various game mechanics used in DS3.
+    /// </summary>
+    public class STAYPARAM : SoulsFile<STAYPARAM>
+    {
+        /// <summary>
+        /// The rows of this stayparam; must be loaded with STAYPARAM.ApplyParamdef() before fields can be read/written.
+        /// </summary>
+        public List<Row> Rows { get; set; }
+
+        /// <summary>
+        /// The current applied STAYPARAMDEF.
+        /// </summary>
+        public STAYPARAMDEF AppliedParamdef { get; private set;  }
+
+        /// <summary>
+        /// A copy of the data in the STAYPARAM, to be parsed after applying a STAYPARAMDEF.
+        /// </summary>
+        private BinaryReaderEx _fieldReader;
+        
+        /// <summary>
+        /// Deserializes file data from a stream.
+        /// </summary>
+        protected override void Read(BinaryReaderEx br)
+        {
+            byte[] copy = br.GetBytes(0, (int) br.Stream.Length);
+            _fieldReader = new BinaryReaderEx(false, copy);
+        }
+
+        /// <summary>
+        /// Serializes file data to a stream.
+        /// </summary>
+        protected override void Write(BinaryWriterEx bw)
+        {
+            if (AppliedParamdef == null)
+                throw new InvalidOperationException("Params cannot be written without applying a paramdef.");
+
+            foreach (var field in Rows)
+            {
+                field.Write(bw);
+            }
+
+            if (bw.Length < 256)
+            {
+                bw.Pad(256);
+            }
+        }
+
+        /// <summary>
+        /// Interprets data according to the given stayparamdef and stores it for later writing.
+        /// </summary>
+        public void ApplyParamdef(STAYPARAMDEF def)
+        {
+            Rows = new List<Row>(def.Fields.Count);
+            
+            foreach (var field in def.Fields)
+            {
+                if (_fieldReader != null)
+                {
+                    Rows.Add(new Row(_fieldReader, field));
+                }
+                else
+                {
+                    Rows.Add(new Row(field));
+                }
+            }
+
+            AppliedParamdef = def;
+        }
+
+        /// <summary>
+        /// Returns the row with the given display name, or null if not found.
+        /// </summary>
+        public Row this[string name] => Rows.Find(row => row.Field.DisplayName.Equals(name, StringComparison.CurrentCultureIgnoreCase));
+        
+        
+        /// <summary>
+        /// An entry of a stayparam that contains a single value and is associated with a STAYPARAMDEF.Field.
+        /// </summary>
+        public class Row
+        {
+            /// <summary>
+            /// The editor settings for the this row.
+            /// </summary>
+            public STAYPARAMDEF.Field Field { get; set; }
+
+            /// <summary>
+            /// The current value of the field in the stayparam.
+            /// </summary>
+            public object Value { get; set; }
+
+            internal Row(STAYPARAMDEF.Field field)
+            {
+                Field = field;
+                Value = field.Default;
+            }
+            
+            internal Row(BinaryReaderEx br, STAYPARAMDEF.Field field)
+            {
+                Field = field;
+
+                if (Field.Type == STAYPARAMDEF.FieldType.s8)
+                {
+                    Value = br.ReadByte();
+                }
+                else if (Field.Type == STAYPARAMDEF.FieldType.s16)
+                {
+                    Value = br.ReadInt16();
+                }
+                else if (Field.Type == STAYPARAMDEF.FieldType.s32)
+                {
+                    Value = br.ReadInt32();
+                }
+                else if (Field.Type == STAYPARAMDEF.FieldType.f32)
+                {
+                    Value = br.ReadSingle();
+                }
+            }
+
+            internal void Write(BinaryWriterEx bw)
+            {
+                if (Field.Type == STAYPARAMDEF.FieldType.s8)
+                {
+                    bw.WriteByte(Convert.ToByte(Value));
+                }
+                else if (Field.Type == STAYPARAMDEF.FieldType.s16)
+                {
+                    bw.WriteInt16(Convert.ToInt16(Value));
+                }
+                else if (Field.Type == STAYPARAMDEF.FieldType.s32)
+                {
+                    bw.WriteInt32(Convert.ToInt32(Value));
+                }
+                else if (Field.Type == STAYPARAMDEF.FieldType.f32)
+                {
+                    bw.WriteSingle(Convert.ToSingle(Value));
+                }
+            }
+        }
+    }
+}
---
+++ SoulsFormats/Formats/PARAM/STAYPARAMDEF.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SoulsFormats
+{
+    /// <summary>
+    /// A companion format to stayparams that describes the field associated with each value. Extension: .stayparamdef
+    /// </summary>
+    public class STAYPARAMDEF : SoulsFile<STAYPARAMDEF>
+    {
+        /// <summary>
+        /// Fields for each value in the STAYPARAM, in order they appear.
+        /// </summary>
+        public List<Field> Fields { get; set; }
+
+        /// <summary>
+        /// Creates a new empty STAYPARAMDEF.
+        /// </summary>
+        public STAYPARAMDEF()
+        {
+            Fields = new List<Field>();
+        }
+
+        /// <summary>
+        /// Deserializes file data from a stream.
+        /// </summary>
+        protected override void Read(BinaryReaderEx br)
+        {
+            int fieldCount = br.ReadInt32();
+            br.AssertInt32(0);
+            br.AssertInt32(0);
+            br.AssertInt32(0);
+            br.Skip(4 * fieldCount);
+
+            Fields = new List<Field>(fieldCount);
+
+            for (int i = 0; i < fieldCount; i++)
+            {
+                Fields.Add(new Field(br));
+            }
+
+            foreach (var field in Fields)
+            {
+                field.ReadStrings(br);
+            }
+        }
+
+        /// <summary>
+        /// Serializes file data to a stream.
+        /// </summary>
+        protected override void Write(BinaryWriterEx bw)
+        {
+            bw.WriteInt32(Fields.Count);
+            bw.WriteInt32(0);
+            bw.WriteInt32(0);
+            bw.WriteInt32(0);
+
+            for (int i = 0; i < Fields.Count; i++)
+            {
+                bw.WriteInt32(0);
+            }
+
+            foreach (var field in Fields)
+            {
+                field.WriteInfo(bw);
+            }
+
+            foreach (var field in Fields)
+            {
+                field.WriteStrings(bw);
+            }
+        }
+
+        /// <summary>
+        /// The data type associated with a Field.
+        /// </summary>
+        public enum FieldType : UInt32
+        {
+            s8 = 0,
+            s16 = 2,
+            s32 = 4,
+            f32 = 6
+        }
+
+        /// <summary>
+        /// The display and numerical properties of a value in a stayparam.
+        /// </summary>
+        public class Field
+        {
+            /// <summary>
+            /// Name to display in the editor.
+            /// </summary>
+            public string DisplayName { get; set; }
+
+            /// <summary>
+            /// Type of the value stored by this field.
+            /// </summary>
+            public FieldType Type { get; set; }
+
+            /// <summary>
+            /// Printf-style format string to apply to the value in the editor.
+            /// </summary>
+            public string DisplayFormat { get; set; }
+
+            /// <summary>
+            /// Default value for new rows.
+            /// </summary>
+            public object Default { get; set; }
+
+            /// <summary>
+            /// Minimum valid value.
+            /// </summary>
+            public object Minimum { get; set; }
+
+            /// <summary>
+            /// Maximum valid value.
+            /// </summary>
+            public object Maximum { get; set; }
+
+            /// <summary>
+            /// Amount of increase or decrease per step when scrolling in the editor.
+            /// </summary>
+            public object Increment { get; set; }
+
+            internal Field(BinaryReaderEx br)
+            {
+                Type = br.ReadEnum32<FieldType>();
+                br.AssertInt64(0);
+                if (Type == FieldType.s8)
+                {
+                    Default = br.ReadByte();
+                    Increment = br.ReadByte();
+                    Minimum = br.ReadByte();
+                    Maximum = br.ReadByte();
+                }
+                else if (Type == FieldType.s16)
+                {
+                    Default = br.ReadInt16();
+                    Increment = br.ReadInt16();
+                    Minimum = br.ReadInt16();
+                    Maximum = br.ReadInt16();
+                }
+                else if (Type == FieldType.s32)
+                {
+                    Default = br.ReadInt32();
+                    Increment = br.ReadInt32();
+                    Minimum = br.ReadInt32();
+                    Maximum = br.ReadInt32();
+                }
+                else if (Type == FieldType.f32)
+                {
+                    Default = br.ReadSingle();
+                    Increment = br.ReadSingle();
+                    Minimum = br.ReadSingle();
+                    Maximum = br.ReadSingle();
+                }
+            }
+
+            internal void ReadStrings(BinaryReaderEx br)
+            {
+                DisplayName = br.ReadASCII();
+                DisplayFormat = br.ReadASCII();
+            }
+
+            internal void WriteInfo(BinaryWriterEx bw)
+            {
+                bw.WriteInt32((int) Type);
+                bw.WriteInt64(0);
+
+                if (Type == FieldType.s8)
+                {
+                    bw.WriteByte(Convert.ToByte(Default));
+                    bw.WriteByte(Convert.ToByte(Increment));
+                    bw.WriteByte(Convert.ToByte(Minimum));
+                    bw.WriteByte(Convert.ToByte(Maximum));
+                }
+                else if (Type == FieldType.s16)
+                {
+                    bw.WriteInt16(Convert.ToInt16(Default));
+                    bw.WriteInt16(Convert.ToInt16(Increment));
+                    bw.WriteInt16(Convert.ToInt16(Minimum));
+                    bw.WriteInt16(Convert.ToInt16(Maximum));
+                }
+                else if (Type == FieldType.s32)
+                {
+                    bw.WriteInt32(Convert.ToInt32(Default));
+                    bw.WriteInt32(Convert.ToInt32(Increment));
+                    bw.WriteInt32(Convert.ToInt32(Minimum));
+                    bw.WriteInt32(Convert.ToInt32(Maximum));
+                }
+                else if (Type == FieldType.f32)
+                {
+                    bw.WriteSingle(Convert.ToSingle(Default));
+                    bw.WriteSingle(Convert.ToSingle(Increment));
+                    bw.WriteSingle(Convert.ToSingle(Minimum));
+                    bw.WriteSingle(Convert.ToSingle(Maximum));
+                }
+            }
+
+            internal void WriteStrings(BinaryWriterEx bw)
+            {
+                bw.WriteASCII(DisplayName);
+                bw.WriteASCII(DisplayFormat);
+            }
+        }
+    }
+}
